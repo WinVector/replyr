dce07278192b98c2003aa697e12cfe99012480b6
John Mount <jmount@win-vector.com>
2016-12-06 10:28:28 -0800

    tighten code

--- R/renameRestrictCols.R
+++ R/renameRestrictCols.R
@@ -2,12 +2,17 @@
 # Contributed by John Mount jmount@win-vector.com , ownership assigned to Win-Vector LLC.
 # Win-Vector LLC currently distributes this code without intellectual property indemnification, warranty, claim of fitness of purpose, or any other guarantee under a GPL3 license.
 
-#' Set names of columns to known values and drop other columns
+#' Map names of columns to known values and drop other columns.
+#'
+#' Used to restrict a data item's column names and re-name them in bulk.  Note: this can be expensive operation.
+#'
+#' Something like \code{replyr::replyr_mapRestrictCols} is only useful to get control of a function that is not parameterized
+#' (in the sense it has hard-coded column names inside its implementation that don't the column names in our data).
 #'
 #' @seealso \code{\link{let}}
 #'
 #' @param x data item to work on
-#' @param nmap named list mapping existing columns to desired new names. Doesn't support permutations of names.
+#' @param nmap named list mapping desired column names to column names in x. Doesn't support permutations of names.
 #' @return data item with columns limitted down to those named in nmap, and re-named.
 #'
 #' @examples
@@ -24,9 +29,11 @@
 #' print(d)
 #'
 #' # map our data to expected column names so we can use function
-#' nmap <- c('GroupColumn','ValueColumn','RankColumn')
-#' names(nmap) <-  c('Species','Sepal_Length','rank')
-#' dm <- replyr_renameRestrictCols(d,nmap)
+#' nmap <- c(GroupColumn='Species',
+#'           ValueColumn='Sepal_Length',
+#'           RankColumn='rank')
+#' print(nmap)
+#' dm <- replyr_mapRestrictCols(d,nmap)
 #' print(dm)
 #'
 #' # can now apply code that expects hard-coded names.
@@ -35,46 +42,47 @@
 #' # map back to our original column names (for the columns we retained)
 #' invmap <- names(nmap)
 #' names(invmap) <- as.character(nmap)
+#' print(invmap)
 #' # Note: can only map back columns that were retained in first mapping.
-#' replyr_renameRestrictCols(dm,invmap)
+#' replyr_mapRestrictCols(dm,invmap)
 #'
 #' @export
-replyr_renameRestrictCols <- function(x,nmap) {
+replyr_mapRestrictCols <- function(x,nmap) {
   nmap <- as.list(nmap)
   if(length(unique(nmap))!=length(nmap)) {
-    stop("replyr::replyr_renameRestrictCols duplicate destination columns in replyr_renameRestrictCols")
+    stop("replyr::replyr_mapRestrictCols duplicate destination columns in replyr_mapRestrictCols")
   }
   if(length(unique(names(nmap)))!=length(nmap)) {
-    stop("replyr::replyr_renameRestrictCols duplicate source columns in replyr_renameRestrictCols")
+    stop("replyr::replyr_mapRestrictCols duplicate source columns in replyr_mapRestrictCols")
   }
   for(ni in names(nmap)) {
     if(!is.character(ni)) {
-      stop('replyr::replyr_renameRestrictCols nmap keys must be strings')
+      stop('replyr::replyr_mapRestrictCols nmap keys must be strings')
     }
     if(length(ni)!=1) {
-      stop('replyr::replyr_renameRestrictCols nmap keys must be strings')
+      stop('replyr::replyr_mapRestrictCols nmap keys must be strings')
     }
     ti <- nmap[[ni]]
     if(!is.character(ti)) {
-      stop('replyr::replyr_renameRestrictCols nmap values must be strings')
+      stop('replyr::replyr_mapRestrictCols nmap values must be strings')
     }
     if(length(ti)!=1) {
-      stop('replyr::replyr_renameRestrictCols nmap values must be strings')
+      stop('replyr::replyr_mapRestrictCols nmap values must be strings')
     }
     if(ti!=ni) {
       if(ti %in% names(nmap)) {
-        stop("replyr::replyr_renameRestrictCols source and destination columns overlap in replyr_renameRestrictCols")
+        stop("replyr::replyr_mapRestrictCols source and destination columns overlap in replyr_mapRestrictCols")
       }
     }
   }
   # limit down to only names we are mapping
   #do.call(dplyr::select_,c(list(x),as.list(names(nmap)))) -> x
-  x %>% dplyr::select(dplyr::one_of(names(nmap))) -> x
+  x %>% dplyr::select(dplyr::one_of(as.character(nmap))) -> x
   # re-map names
   for(ni in names(nmap)) {
     ti <- nmap[[ni]]
     if(ti!=ni) {
-      x %>% dplyr::rename_(.dots=stats::setNames(ni,ti)) -> x
+      x %>% dplyr::rename_(.dots=stats::setNames(ti,ni)) -> x
     }
   }
   x
