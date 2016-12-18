08d27ed99b6fbb2aba8b571c4b6c26c826f5c840
John Mount <jmount@win-vector.com>
2016-12-18 02:15:09 -0800

    Check for valid names. Document eager eval.

--- R/land.R
+++ R/land.R
@@ -7,6 +7,7 @@
 #' a variable named by its RHS argument.  There is nothing these operators do
 #' better than "->" and they are mostly just a proof of concept (though technically they
 #' are not "-> assignment" so they may not be specifically prohibited in some style guides).
+#' These operators use eager evaluation.
 #'
 #' @param value value to write
 #' @param name variable to write to
@@ -23,6 +24,11 @@
 #' @export
 `%land%` <- function(value, name) {
   name <- as.character(substitute(name))
+  if((length(name)!=1)||(!is.character(name))||
+     (!isValidAndUnreservedName(name))) {
+    stop("replyr::`%land%` name argument must be a valid potential variable name")
+  }
+  force(value)
   envir <- parent.frame(1)
   assign(name, value,
          pos = envir,
@@ -34,6 +40,11 @@
 #' @rdname grapes-land-grapes
 `%->%` <- function(value, name) {
   name <- as.character(substitute(name))
+  if((length(name)!=1)||(!is.character(name))||
+     (!isValidAndUnreservedName(name))) {
+    stop("replyr::`%->%` name argument must be a valid potential variable name")
+  }
+  force(value)
   envir <- parent.frame(1)
   assign(name, value,
          pos = envir,
@@ -44,6 +55,11 @@
 #' @export
 #' @rdname grapes-land-grapes
 `%->_%` <- function(value, name) {
+  if((length(name)!=1)||(!is.character(name))||
+     (!isValidAndUnreservedName(name))) {
+    stop("replyr::`%->_%` name argument must be a valid potential variable name")
+  }
+  force(value)
   envir <- parent.frame(1)
   assign(name, value,
          pos = envir,
@@ -54,6 +70,11 @@
 #' @export
 #' @rdname grapes-land-grapes
 `%land_%` <- function(value, name) {
+  if((length(name)!=1)||(!is.character(name))||
+     (!isValidAndUnreservedName(name))) {
+    stop("replyr::`%land_%` name argument must be a valid potential variable name")
+  }
+  force(value)
   envir <- parent.frame(1)
   assign(name, value,
          pos = envir,
