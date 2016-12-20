1fdfedf19fdadc7b6f50495774fc9f59279a55f7
John Mount <jmount@win-vector.com>
2016-12-20 11:19:04 -0800

    Found the word

--- R/land.R
+++ R/land.R
@@ -12,7 +12,7 @@
 #'
 #' Technically these operators are
 #' are not "-> assignment", so they might not be specifically prohibited in a
-#' pedantic reading of some style guides.
+#' oppugnant reading of some style guides.
 #'
 #' @param value value to write
 #' @param name variable to write to
