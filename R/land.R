60d2b99fd61bae16e20d01a568de0d4e62de1767
John Mount <jmount@win-vector.com>
2016-12-23 09:11:21 -0800

    typo

--- R/land.R
+++ R/land.R
@@ -11,7 +11,7 @@
 #'
 #'
 #' Technically these operators are
-#' are not "-> assignment", so they might not be specifically prohibited in a
+#' not "-> assignment", so they might not be specifically prohibited in a
 #' oppugnant reading of some style guides.
 #'
 #' @param value value to write
