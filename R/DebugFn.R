cc5180015f1fd680520eac002b227622b05ac0b1
John Mount <jmount@win-vector.com>
2017-02-04 13:42:17 -0800

    Allow general value return

--- R/DebugFn.R
+++ R/DebugFn.R
@@ -1,59 +1,83 @@
 
+
+# return an error to a file or callback
+returnCapture <- function(e, saveDest, cap, wrapperName) {
+  if(is.null(saveDest)) {
+    saveDest <- paste0(tempfile('debug'),'.RDS')
+  }
+  if(is.function(saveDest)) {
+    saveDest(cap)
+    fName <- attr(saveDest, 'name')
+    if(!is.null(fName)) {
+      stop(paste0("replyr::", wrapperName, ": wrote error to user function: ",
+                  fName))
+    }
+    stop(paste0("replyr::", wrapperName, ": wrote error to user function"))
+  }
+  if(is.character(saveDest)) {
+    saveRDS(object=cap,
+            file=saveDest)
+    stop(paste0("replyr::", wrapperName, ": wrote '",saveDest,
+                "' on catching '",as.character(e),"'",
+                "\n You can reproduce the error with:",
+                "\n'p <- readRDS('",saveDest,
+                "'); do.call(p$fn, p$args)'"))
+  }
+  stop(paste0("replyr::", wrapperName, ": don't know how to write error to ",
+              class(saveDest)))
+}
+
+
 #' Capture arguments of exception throwing function call for later debugging.
 #'
 #' Run fn, save arguments on failure.
 #' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
-#' @param saveFile path to save RDS to.
+#' @param saveDest path to save RDS or function to pass argument to for saving.
 #' @param fn function to call
 #' @param ... arguments for fn
-#' @return fn(...) normally, but if fn(...) throws an exception save to saveFile RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
+#' @return fn(...) normally, but if fn(...) throws an exception save to saveDest RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
 #'
 #' @examples
 #'
-#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' saveDest <- paste0(tempfile('debug'),'.RDS')
 #' f <- function(i) { (1:10)[[i]] }
 #' # correct run
-#' DebugFn(saveName, f, 5)
+#' DebugFn(saveDest, f, 5)
 #' # now re-run
 #' # capture error on incorrect run
 #' tryCatch(
-#'    DebugFn(saveName, f, 12),
+#'    DebugFn(saveDest, f, 12),
 #'    error = function(e) { print(e) })
 #' # examine details
-#' situation <- readRDS(saveName)
+#' situation <- readRDS(saveDest)
 #' str(situation)
 #' # fix and re-run
 #' situation$args[[1]] <- 6
 #' do.call(situation$fn,situation$args)
 #' # clean up
-#' file.remove(saveName)
+#' file.remove(saveDest)
 #'
 #' @export
-DebugFn <- function(saveFile,fn,...) {
+DebugFn <- function(saveDest,fn,...) {
   args <- list(...)
   envir = parent.frame()
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
-  force(saveFile)
+  force(saveDest)
   force(fn)
   tryCatch({
     do.call(fn, args, envir=envir)
   },
   error = function(e) {
-    if(is.null(saveFile)) {
-      saveFile <- paste0(tempfile('debug'),'.RDS')
+    if(is.null(saveDest)) {
+      saveDest <- paste0(tempfile('debug'),'.RDS')
     }
-    saveRDS(object=list(fn=fn,
-                        args=args,
-                        fn_name=fn_name),
-            file=saveFile)
-    stop(paste0("replyr::DebugFn: wrote '",saveFile,
-                "' on catching '",as.character(e),"'",
-                "\n You can reproduce the error with:",
-                "\n'p <- readRDS('",saveFile,
-                "'); do.call(p$fn, p$args)'"))
-    })
+    cap <- list(fn=fn,
+                args=args,
+                fn_name=fn_name)
+    returnCapture(e, saveDest, cap, "DebugFn")
+  })
 }
 
 #' Wrap a function for debugging.
@@ -63,15 +87,15 @@ DebugFn <- function(saveFile,fn,...) {
 #'
 #' Idea from: https://gist.github.com/nassimhaddad/c9c327d10a91dcf9a3370d30dff8ac3d
 #'
-#' @param saveFile path to save RDS to.
+#' @param saveDest path to save RDS or function to pass argument to for saving.
 #' @param fn function to call
 #' @return wrapped function that saves state on error.
 #'
 #' @examples
 #'
-#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' saveDest <- paste0(tempfile('debug'),'.RDS')
 #' f <- function(i) { (1:10)[[i]] }
-#' df <- DebugFnW(saveName,f)
+#' df <- DebugFnW(saveDest,f)
 #' # correct run
 #' df(5)
 #' # now re-run
@@ -80,19 +104,33 @@ DebugFn <- function(saveFile,fn,...) {
 #'    df(12),
 #'    error = function(e) { print(e) })
 #' # examine details
-#' situation <- readRDS(saveName)
+#' situation <- readRDS(saveDest)
 #' str(situation)
 #' # fix and re-run
 #' situation$args[[1]] <- 6
 #' do.call(situation$fn,situation$args)
 #' # clean up
-#' file.remove(saveName)
+#' file.remove(saveDest)
+#'
+#'
+#' f <- function(i) { (1:10)[[i]] }
+#' curEnv <- environment()
+#' writeBack <- function(sit) {
+#'    assign('lastError', sit, envir=curEnv)
+#' }
+#' attr(writeBack,'name') <- 'writeBack'
+#' df <- DebugFnW(writeBack,f)
+#' tryCatch(
+#'    df(12),
+#'    error = function(e) { print(e) })
+#' str(lastError)
+#'
 #'
 #' @export
-DebugFnW <- function(saveFile,fn) {
+DebugFnW <- function(saveDest,fn) {
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
-  force(saveFile)
+  force(saveDest)
   force(fn)
   function(...) {
     args <- list(...)
@@ -102,19 +140,11 @@ DebugFnW <- function(saveFile,fn) {
       do.call(fn, args, envir=envir)
     },
     error = function(e) {
-      if(is.null(saveFile)) {
-        saveFile <- paste0(tempfile('debug'),'.RDS')
-      }
-      saveRDS(object=list(fn=fn,
-                          args=args,
-                          namedargs=namedargs,
-                          fn_name=fn_name),
-              file=saveFile)
-      stop(paste0("replyr::DebugFnW: wrote '",saveFile,
-                  "' on catching '",as.character(e),"'",
-                  "\n You can reproduce the error with:",
-                  "\n'p <- readRDS('",saveFile,
-                  "'); do.call(p$fn, p$args)'"))
+      cap <- list(fn=fn,
+                  args=args,
+                  namedargs=namedargs,
+                  fn_name=fn_name)
+      returnCapture(e, saveDest, cap, "DebugFnW")
     })
   }
 }
@@ -127,38 +157,38 @@ DebugFnW <- function(saveFile,fn) {
 #'
 #' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
-#' @param saveFile path to save RDS to.
+#' @param saveDest path to save RDS or function to pass argument to for saving.
 #' @param fn function to call
 #' @param ... arguments for fn
-#' @return fn(...) normally, but if fn(...) throws an exception save to saveFile RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
+#' @return fn(...) normally, but if fn(...) throws an exception save to saveDest RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
 #'
 #' @examples
 #'
-#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' saveDest <- paste0(tempfile('debug'),'.RDS')
 #' f <- function(i) { (1:10)[[i]] }
 #' # correct run
-#' DebugPrintFn(saveName, f, 5)
+#' DebugPrintFn(saveDest, f, 5)
 #' # now re-run
 #' # capture error on incorrect run
 #' tryCatch(
-#'    DebugPrintFn(saveName, f, 12),
+#'    DebugPrintFn(saveDest, f, 12),
 #'    error = function(e) { print(e) })
 #' # examine details
-#' situation <- readRDS(saveName)
+#' situation <- readRDS(saveDest)
 #' str(situation)
 #' # fix and re-run
 #' situation$args[[1]] <- 6
 #' do.call(situation$fn,situation$args)
 #' # clean up
-#' file.remove(saveName)
+#' file.remove(saveDest)
 #'
 #' @export
-DebugPrintFn <- function(saveFile,fn,...) {
+DebugPrintFn <- function(saveDest,fn,...) {
   args <- list(...)
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
   envir = parent.frame()
-  force(saveFile)
+  force(saveDest)
   force(fn)
   tryCatch({
     res = do.call(fn, args, envir=envir)
@@ -166,18 +196,10 @@ DebugPrintFn <- function(saveFile,fn,...) {
     res
   },
   error = function(e) {
-    if(is.null(saveFile)) {
-      saveFile <- paste0(tempfile('debug'),'.RDS')
-    }
-    saveRDS(object=list(fn=fn,
-                        args=args,
-                        fn_name=fn_name),
-            file=saveFile)
-    stop(paste0("replyr::DebugPrintFn: wrote '",saveFile,
-                "' on catching '",as.character(e),"'",
-                "\n You can reproduce the error with:",
-                "\n'p <- readRDS('",saveFile,
-                "'); do.call(p$fn, p$args)'"))
+    cap <- list(fn=fn,
+                args=args,
+                fn_name=fn_name)
+    returnCapture(e, saveDest, cap, "DebugPrintFn")
   })
 }
 
@@ -186,56 +208,48 @@ DebugPrintFn <- function(saveFile,fn,...) {
 #' Run fn, save arguments on failure.
 #' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
-#' @param saveFile path to save RDS to.
+#' @param saveDest path to save RDS or function to pass argument to for saving.
 #' @param fn function to call
 #' @param ... arguments for fn
-#' @return fn(...) normally, but if fn(...) throws an exception save to saveFile RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
+#' @return fn(...) normally, but if fn(...) throws an exception save to saveDest RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
 #'
 #' @examples
 #'
-#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' saveDest <- paste0(tempfile('debug'),'.RDS')
 #' f <- function(i) { (1:10)[[i]] }
 #' # correct run
-#' DebugFnE(saveName, f, 5)
+#' DebugFnE(saveDest, f, 5)
 #' # now re-run
 #' # capture error on incorrect run
 #' tryCatch(
-#'    DebugFnE(saveName, f, 12),
+#'    DebugFnE(saveDest, f, 12),
 #'    error = function(e) { print(e) })
 #' # examine details
-#' situation <- readRDS(saveName)
+#' situation <- readRDS(saveDest)
 #' str(situation)
 #' # fix and re-run
 #' situation$args[[1]] <- 6
 #' do.call(situation$fn, situation$args, envir=situation$env)
 #' # clean up
-#' file.remove(saveName)
+#' file.remove(saveDest)
 #'
 #' @export
-DebugFnE <- function(saveFile,fn,...) {
+DebugFnE <- function(saveDest,fn,...) {
   args <- list(...)
   envir = parent.frame()
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
-  force(saveFile)
+  force(saveDest)
   force(fn)
   tryCatch({
     do.call(fn, args, envir=envir)
   },
   error = function(e) {
-    if(is.null(saveFile)) {
-      saveFile <- paste0(tempfile('debug'),'.RDS')
-    }
-    saveRDS(object=list(fn=fn,
-                        args=args,
-                        env=envir,
-                        fn_name=fn_name),
-            file=saveFile)
-    stop(paste0("replyr::DebugFnE: wrote '",saveFile,
-                "' on catching '",as.character(e),"'",
-                "\n You can reproduce the error with:",
-                "\n'p <- readRDS('",saveFile,
-                "'); do.call(p$fn, p$args, envir=p$env)'"))
+    cap <- list(fn=fn,
+                args=args,
+                env=envir,
+                fn_name=fn_name)
+    returnCapture(e, saveDest, cap, "DebugFnE")
   })
 }
 
@@ -247,16 +261,16 @@ DebugFnE <- function(saveFile,fn,...) {
 #'
 #' Idea from: https://gist.github.com/nassimhaddad/c9c327d10a91dcf9a3370d30dff8ac3d
 #'
-#' @param saveFile path to save RDS to.
+#' @param saveDest path to save RDS or function to pass argument to for saving.
 #' @param fn function to call
 #' @param ... arguments for fn
 #' @return wrapped function that captures state on error.
 #'
 #' @examples
 #'
-#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' saveDest <- paste0(tempfile('debug'),'.RDS')
 #' f <- function(i) { (1:10)[[i]] }
-#' df <- DebugFnWE(saveName, f)
+#' df <- DebugFnWE(saveDest, f)
 #' # correct run
 #' df(5)
 #' # now re-run
@@ -265,19 +279,19 @@ DebugFnE <- function(saveFile,fn,...) {
 #'    df(12),
 #'    error = function(e) { print(e) })
 #' # examine details
-#' situation <- readRDS(saveName)
+#' situation <- readRDS(saveDest)
 #' str(situation)
 #' # fix and re-run
 #' situation$args[[1]] <- 6
 #' do.call(situation$fn, situation$args, envir=situation$env)
 #' # clean up
-#' file.remove(saveName)
+#' file.remove(saveDest)
 #'
 #' @export
-DebugFnWE <- function(saveFile,fn,...) {
+DebugFnWE <- function(saveDest,fn,...) {
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
-  force(saveFile)
+  force(saveDest)
   force(fn)
   function(...) {
     args <- list(...)
@@ -287,20 +301,12 @@ DebugFnWE <- function(saveFile,fn,...) {
       do.call(fn, args, envir=envir)
     },
     error = function(e) {
-      if(is.null(saveFile)) {
-        saveFile <- paste0(tempfile('debug'),'.RDS')
-      }
-      saveRDS(object=list(fn=fn,
-                          args=args,
-                          namedargs=namedargs,
-                          fn_name=fn_name,
-                          env=envir),
-              file=saveFile)
-      stop(paste0("replyr::DebugFnWE: wrote '",saveFile,
-                  "' on catching '",as.character(e),"'",
-                  "\n You can reproduce the error with:",
-                  "\n'p <- readRDS('",saveFile,
-                  "'); do.call(p$fn, p$args, envir=p$env)'"))
+      cap <- list(fn=fn,
+                  args=args,
+                  namedargs=namedargs,
+                  fn_name=fn_name,
+                  env=envir)
+      returnCapture(e, saveDest, cap, "DebugFnWE")
     })
   }
 }
@@ -312,38 +318,38 @@ DebugFnWE <- function(saveFile,fn,...) {
 #'
 #' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
-#' @param saveFile path to save RDS to.
+#' @param saveDest path to save RDS or function to pass argument to for saving.
 #' @param fn function to call
 #' @param ... arguments for fn
-#' @return fn(...) normally, but if fn(...) throws an exception save to saveFile RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
+#' @return fn(...) normally, but if fn(...) throws an exception save to saveDest RDS of list r such that do.call(r$fn,r$args) repeats the call to fn with args.
 #'
 #' @examples
 #'
-#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' saveDest <- paste0(tempfile('debug'),'.RDS')
 #' f <- function(i) { (1:10)[[i]] }
 #' # correct run
-#' DebugPrintFnE(saveName, f, 5)
+#' DebugPrintFnE(saveDest, f, 5)
 #' # now re-run
 #' # capture error on incorrect run
 #' tryCatch(
-#'    DebugPrintFnE(saveName, f, 12),
+#'    DebugPrintFnE(saveDest, f, 12),
 #'    error = function(e) { print(e) })
 #' # examine details
-#' situation <- readRDS(saveName)
+#' situation <- readRDS(saveDest)
 #' str(situation)
 #' # fix and re-run
 #' situation$args[[1]] <- 6
 #' do.call(situation$fn, situation$args, envir=situation$env)
 #' # clean up
-#' file.remove(saveName)
+#' file.remove(saveDest)
 #'
 #' @export
-DebugPrintFnE <- function(saveFile,fn,...) {
+DebugPrintFnE <- function(saveDest,fn,...) {
   args <- list(...)
   envir = parent.frame()
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
-  force(saveFile)
+  force(saveDest)
   force(fn)
   tryCatch({
     res = do.call(fn, args, envir=envir)
@@ -351,19 +357,11 @@ DebugPrintFnE <- function(saveFile,fn,...) {
     res
   },
   error = function(e) {
-    if(is.null(saveFile)) {
-      saveFile <- paste0(tempfile('debug'),'.RDS')
-    }
-    saveRDS(object=list(fn=fn,
-                        args=args,
-                        env=envir,
-                        fn_name=fn_name),
-            file=saveFile)
-    stop(paste0("replyr::DebugPrintFnE: wrote '",saveFile,
-                "' on catching '",as.character(e),"'",
-                "\n You can reproduce the error with:",
-                "\n'p <- readRDS('",saveFile,
-                "'); do.call(p$fn, p$args, envir=p$env)'"))
+    cap <- list(fn=fn,
+                args=args,
+                env=envir,
+                fn_name=fn_name)
+    returnCapture(e, saveDest, cap, "DebugPrintFnE")
   })
 }
 
