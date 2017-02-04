40793cb53ba44bc70e55cd16c64fdfa812eeb553
John Mount <jmount@win-vector.com>
2017-02-04 11:20:29 -0800

    Add wrapping version of debug

--- R/DebugFn.R
+++ R/DebugFn.R
@@ -2,7 +2,7 @@
 #' Capture arguments of exception throwing function call for later debugging.
 #'
 #' Run fn, save arguments on failure.
-#' @seealso \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
+#' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
 #' @param saveFile path to save RDS to.
 #' @param fn function to call
@@ -37,13 +37,11 @@ DebugFn <- function(saveFile,fn,...) {
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
   tryCatch({
-    res = do.call(fn, args, envir=envir)
-    res
+    do.call(fn, args, envir=envir)
   },
   error = function(e) {
     saveRDS(object=list(fn=fn,
                         args=args,
-                        namedargs=namedargs,
                         fn_name=fn_name),
             file=saveFile)
     stop(paste0("replyr::DebugFn: wrote '",saveFile,
@@ -54,12 +52,70 @@ DebugFn <- function(saveFile,fn,...) {
     })
 }
 
+#' Wrap a function for debugging.
+#'
+#' Run fn, save arguments on failure.
+#' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
+#'
+#' Idea from: https://gist.github.com/nassimhaddad/c9c327d10a91dcf9a3370d30dff8ac3d
+#'
+#' @param saveFile path to save RDS to.
+#' @param fn function to call
+#' @return wrapped function that saves state on error.
+#'
+#' @examples
+#'
+#' d <- data.frame(x=1:5)
+#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' f <- function(i) { (1:10)[[i]] }
+#' df <- DebugFnW(saveName,f)
+#' # correct run
+#' df(5)
+#' # now re-run
+#' # capture error on incorrect run
+#' tryCatch(
+#'    df(12),
+#'    error = function(e) { print(e) })
+#' # examine details
+#' situation <- readRDS(saveName)
+#' str(situation)
+#' # fix and re-run
+#' situation$args[[1]] <- 6
+#' do.call(situation$fn,situation$args)
+#' # clean up
+#' file.remove(saveName)
+#'
+#' @export
+DebugFnW <- function(saveFile,fn) {
+  namedargs <- match.call()
+  fn_name <- as.character(namedargs[['fn']])
+  function(...) {
+    args <- list(...)
+    envir = parent.frame()
+    tryCatch({
+      do.call(fn, args, envir=envir)
+    },
+    error = function(e) {
+      saveRDS(object=list(fn=fn,
+                          args=args,
+                          fn_name=fn_name),
+              file=saveFile)
+      stop(paste0("replyr::DebugFnW: wrote '",saveFile,
+                  "' on catching '",as.character(e),"'",
+                  "\n You can reproduce the error with:",
+                  "\n'p <- readRDS('",saveFile,
+                  "'); do.call(p$fn, p$args)'"))
+    })
+  }
+}
+
+
 #' Capture arguments of exception throwing function call for later debugging.
 #'
 #' Run fn and print result, save arguments on failure.  Use on systems like ggplot()
 #' where some calculation is delayed until print().
 #'
-#' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
+#' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
 #' @param saveFile path to save RDS to.
 #' @param fn function to call
@@ -101,7 +157,6 @@ DebugPrintFn <- function(saveFile,fn,...) {
   error = function(e) {
     saveRDS(object=list(fn=fn,
                         args=args,
-                        namedargs=namedargs,
                         fn_name=fn_name),
             file=saveFile)
     stop(paste0("replyr::DebugPrintFn: wrote '",saveFile,
@@ -115,7 +170,7 @@ DebugPrintFn <- function(saveFile,fn,...) {
 #' Capture arguments and environment of exception throwing function call for later debugging.
 #'
 #' Run fn, save arguments on failure.
-#' @seealso \code{\link{DebugFn}}, \code{\link{DebugPrintFn}}, \code{\link{DebugPrintFnE}}
+#' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
 #' @param saveFile path to save RDS to.
 #' @param fn function to call
@@ -150,14 +205,12 @@ DebugFnE <- function(saveFile,fn,...) {
   namedargs <- match.call()
   fn_name <- as.character(namedargs[['fn']])
   tryCatch({
-    res = do.call(fn, args, envir=envir)
-    res
+    do.call(fn, args, envir=envir)
   },
   error = function(e) {
     saveRDS(object=list(fn=fn,
                         args=args,
                         env=envir,
-                        namedargs=namedargs,
                         fn_name=fn_name),
             file=saveFile)
     stop(paste0("replyr::DebugFnE: wrote '",saveFile,
@@ -168,12 +221,72 @@ DebugFnE <- function(saveFile,fn,...) {
   })
 }
 
+
+#' Wrap function to capture arguments and environment of exception throwing function call for later debugging.
+#'
+#' Run fn, save arguments on failure.
+#' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
+#'
+#' Idea from: https://gist.github.com/nassimhaddad/c9c327d10a91dcf9a3370d30dff8ac3d
+#'
+#' @param saveFile path to save RDS to.
+#' @param fn function to call
+#' @param ... arguments for fn
+#' @return wrapped function that captures state on error.
+#'
+#' @examples
+#'
+#' d <- data.frame(x=1:5)
+#' saveName <- paste0(tempfile('debug'),'.RDS')
+#' f <- function(i) { (1:10)[[i]] }
+#' df <- DebugFnWE(saveName, f)
+#' # correct run
+#' df(5)
+#' # now re-run
+#' # capture error on incorrect run
+#' tryCatch(
+#'    df(12),
+#'    error = function(e) { print(e) })
+#' # examine details
+#' situation <- readRDS(saveName)
+#' str(situation)
+#' # fix and re-run
+#' situation$args[[1]] <- 6
+#' do.call(situation$fn, situation$args, envir=situation$env)
+#' # clean up
+#' file.remove(saveName)
+#'
+#' @export
+DebugFnWE <- function(saveFile,fn,...) {
+  namedargs <- match.call()
+  fn_name <- as.character(namedargs[['fn']])
+  function(...) {
+    args <- list(...)
+    envir = parent.frame()
+    tryCatch({
+      do.call(fn, args, envir=envir)
+    },
+    error = function(e) {
+      saveRDS(object=list(fn=fn,
+                          args=args,
+                          env=envir,
+                          fn_name=fn_name),
+              file=saveFile)
+      stop(paste0("replyr::DebugFnWE: wrote '",saveFile,
+                  "' on catching '",as.character(e),"'",
+                  "\n You can reproduce the error with:",
+                  "\n'p <- readRDS('",saveFile,
+                  "'); do.call(p$fn, p$args, envir=p$env)'"))
+    })
+  }
+}
+
 #' Capture arguments and environment of exception throwing function call for later debugging.
 #'
 #' Run fn and print result, save arguments on failure.  Use on systems like ggplot()
 #' where some calculation is delayed until print().
 #'
-#' @seealso \code{\link{DebugFn}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}
+#' @seealso \code{\link{DebugFn}}, \code{\link{DebugFnW}},  \code{\link{DebugFnWE}}, \code{\link{DebugPrintFn}}, \code{\link{DebugFnE}}, \code{\link{DebugPrintFnE}}
 #'
 #' @param saveFile path to save RDS to.
 #' @param fn function to call
@@ -216,7 +329,6 @@ DebugPrintFnE <- function(saveFile,fn,...) {
     saveRDS(object=list(fn=fn,
                         args=args,
                         env=envir,
-                        namedargs=namedargs,
                         fn_name=fn_name),
             file=saveFile)
     stop(paste0("replyr::DebugPrintFnE: wrote '",saveFile,
@@ -226,3 +338,4 @@ DebugPrintFnE <- function(saveFile,fn,...) {
                 "'); do.call(p$fn, p$args, envir=p$env)'"))
   })
 }
+
