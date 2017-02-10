708b278c6d90848b712502a5ffcfbbcd243694cf
John Mount <jmount@win-vector.com>
2017-02-10 09:34:21 -0800

    re-expor wrapr debug

---
+++ R/debug.R
@@ -0,0 +1,31 @@
+
+
+#' @importFrom wrapr DebugFn
+#' @export
+wrapr::DebugFn
+
+#' @importFrom wrapr DebugFnE
+#' @export
+wrapr::DebugFnE
+
+#' @importFrom wrapr DebugFnW
+#' @export
+wrapr::DebugFnW
+
+#' @importFrom wrapr DebugFnWE
+#' @export
+wrapr::DebugFnWE
+
+#' @importFrom wrapr DebugPrintFn
+#' @export
+wrapr::DebugPrintFn
+
+#' @importFrom wrapr DebugPrintFnE
+#' @export
+wrapr::DebugPrintFnE
+
+
+
+
+
+
