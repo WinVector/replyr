3cfc5fe20bad874b7cfd6fe9c7b825b54a2f8bce
John Mount <jmount@win-vector.com>
2017-02-10 20:05:54 -0800

    edit

--- R/replyr.R
+++ R/replyr.R
@@ -4,10 +4,9 @@
 #' Spark) through dplyr.  The idea is to add convenience functions to make such task more like
 #' working with an in-memory data.frame.  Results do depend on which dplyr service you use.
 #'
-#' replyr has the following:
+#' \code{replyr} helps with the following:
 #'
 #' \itemize{
-#' \item Make "parametric treatment of variable names" easier through the \code{\link{let}} command.
 #' \item Package common data manipulation tasks into operators  such as the \code{\link{gapply}} function.
 #' \item Provide "remote data" (SQL, Spark) replacements for functions commonly used on in-memory data frames.
 #' \item Provide bug-fixes and work-arounds for various data services.
