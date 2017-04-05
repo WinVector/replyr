363384b3ebf7e66134ba3ff3437b40e4a9ee3493
John Mount <jmount@win-vector.com>
2017-04-04 17:31:48 -0700

    better spread simulation

--- R/rowscols.R
+++ R/rowscols.R
@@ -2,41 +2,41 @@
 # Contributed by John Mount jmount@win-vector.com , ownership assigned to Win-Vector LLC.
 # Win-Vector LLC currently distributes this code without intellectual property indemnification, warranty, claim of fitness of purpose, or any other guarantee under a GPL3 license.
 
-# @importFrom dplyr select mutate one_of
+#' @importFrom dplyr select mutate one_of
 NULL
 
-# dplyr::one_of is what is causing us to depend on dplyr (>= 0.5.0)
+#' dplyr::one_of is what is causing us to depend on dplyr (>= 0.5.0)
 
-# Collect values found in columnsToTakeFrom as tuples (experimental, not fully tested on multiple data suppliers)
-#
-# Collect values found in columnsToTakeFrom as tuples naming which column the value came from (placed in nameForNewKeyColumn)
-# and value found (placed in nameForNewValueColumn).  This is essentially a tidyr::gather, dplyr::melt, or anti-pivot.
-#
-# @param data data.frame to work with.
-# @param nameForNewKeyColumn character name of column to write new keys in.
-# @param nameForNewValueColumn character name of column to write new values in.
-# @param columnsToTakeFrom character array names of columns to take values from.
-# @param eagerCompute if TRUE call compute on intermediate results
-# @return data item
-#
-# @examples
-#
-# d <- data.frame(
-#   index = c(1, 2, 3),
-#   info = c('a', 'b', 'c'),
-#   meas1 = c('m1_1', 'm1_2', 'm1_3'),
-#   meas2 = c('m2_1', 'm2_2', 'm2_3'),
-#   stringsAsFactors = FALSE)
-# replyr_moveValuesToRows(d,
-#               nameForNewKeyColumn= 'meastype',
-#               nameForNewValueColumn= 'meas',
-#               columnsToTakeFrom= c('meas1','meas2'))
-# # cdata::moveValuesToRows(d,
-# #               nameForNewKeyColumn= 'meastype',
-# #               nameForNewValueColumn= 'meas',
-# #               columnsToTakeFrom= c('meas1','meas2'))
-#
-# @export
+#' Collect values found in columnsToTakeFrom as tuples (experimental, not fully tested on multiple data suppliers)
+#'
+#' Collect values found in columnsToTakeFrom as tuples naming which column the value came from (placed in nameForNewKeyColumn)
+#' and value found (placed in nameForNewValueColumn).  This is essentially a tidyr::gather, dplyr::melt, or anti-pivot.
+#'
+#' @param data data.frame to work with.
+#' @param nameForNewKeyColumn character name of column to write new keys in.
+#' @param nameForNewValueColumn character name of column to write new values in.
+#' @param columnsToTakeFrom character array names of columns to take values from.
+#' @param eagerCompute if TRUE call compute on intermediate results
+#' @return data item
+#'
+#' @examples
+#'
+#' d <- data.frame(
+#'   index = c(1, 2, 3),
+#'   info = c('a', 'b', 'c'),
+#'   meas1 = c('m1_1', 'm1_2', 'm1_3'),
+#'   meas2 = c('m2_1', 'm2_2', 'm2_3'),
+#'   stringsAsFactors = FALSE)
+#' replyr_moveValuesToRows(d,
+#'               nameForNewKeyColumn= 'meastype',
+#'               nameForNewValueColumn= 'meas',
+#'               columnsToTakeFrom= c('meas1','meas2'))
+#' # cdata::moveValuesToRows(d,
+#' #               nameForNewKeyColumn= 'meastype',
+#' #               nameForNewValueColumn= 'meas',
+#' #               columnsToTakeFrom= c('meas1','meas2'))
+#'
+#' @export
 replyr_moveValuesToRows <- function(data,
                                     nameForNewKeyColumn,
                                     nameForNewValueColumn,
@@ -90,7 +90,6 @@ replyr_moveValuesToRows <- function(data,
         dtmp %>% dplyr::mutate(NEWCOL='OLDCOL') -> dtmp
       )
     }
-    print('.')
     wrapr::let(
       c(NEWCOL=nameForNewValueColumn, OLDCOL=di),
       dtmp %>%
@@ -108,41 +107,41 @@ replyr_moveValuesToRows <- function(data,
 
 
 
-# Spread values found in rowKeyColumns row groups as new columns (experimental, not fully tested on multiple data suppliers)
-#
-# Spread values found in rowKeyColumns row groups as new columns.
-# Values types (new column names) are identified in nameForNewKeyColumn and values are taken
-# from nameForNewValueColumn.
-# This is denormalizing operation, or essentially a tidyr::spread, dplyr::dcast, or pivot.
-# This implementation moves
-# so much data it is essentially working locally and also very inefficient.
-#
-# @param data data.frame to work with.
-# @param columnToTakeKeysFrom character name of column build new column names from.
-# @param columnToTakeValuesFrom character name of column to get values from.
-# @param rowKeyColumns character array names columns that should be table keys.
-# @param maxcols maximum number of values to expand to columns
-# @param eagerCompute if TRUE call compute on intermediate results
-# @return data item
-#
-# @examples
-#
-# d <- data.frame(
-#   index = c(1, 2, 3, 1, 2, 3),
-#   meastype = c('meas1','meas1','meas1','meas2','meas2','meas2'),
-#   meas = c('m1_1', 'm1_2', 'm1_3', 'm2_1', 'm2_2', 'm2_3'),
-#   stringsAsFactors = FALSE)
-# replyr_moveValuesToColumns(d,
-#                            columnToTakeKeysFrom= 'meastype',
-#                            columnToTakeValuesFrom= 'meas',
-#                            rowKeyColumns= 'index')
-# # cdata::moveValuesToColumns(d,
-# #                            columnToTakeKeysFrom= 'meastype',
-# #                            columnToTakeValuesFrom= 'meas',
-# #                            rowKeyColumns= 'index')
-#
-#
-# @export
+#' Spread values found in rowKeyColumns row groups as new columns (experimental, not fully tested on multiple data suppliers)
+#'
+#' Spread values found in rowKeyColumns row groups as new columns.
+#' Values types (new column names) are identified in nameForNewKeyColumn and values are taken
+#' from nameForNewValueColumn.
+#' This is denormalizing operation, or essentially a tidyr::spread, dplyr::dcast, or pivot.
+#' This implementation moves
+#' so much data it is essentially working locally and also very inefficient.
+#'
+#' @param data data.frame to work with.
+#' @param columnToTakeKeysFrom character name of column build new column names from.
+#' @param columnToTakeValuesFrom character name of column to get values from.
+#' @param rowKeyColumns character array names columns that should be table keys.
+#' @param maxcols maximum number of values to expand to columns
+#' @param eagerCompute if TRUE call compute on intermediate results
+#' @return data item
+#'
+#' @examples
+#'
+#' d <- data.frame(
+#'   index = c(1, 2, 3, 1, 2, 3),
+#'   meastype = c('meas1','meas1','meas1','meas2','meas2','meas2'),
+#'   meas = c('m1_1', 'm1_2', 'm1_3', 'm2_1', 'm2_2', 'm2_3'),
+#'   stringsAsFactors = FALSE)
+#' replyr_moveValuesToColumns(d,
+#'                            columnToTakeKeysFrom= 'meastype',
+#'                            columnToTakeValuesFrom= 'meas',
+#'                            rowKeyColumns= 'index')
+#' # cdata::moveValuesToColumns(d,
+#' #                            columnToTakeKeysFrom= 'meastype',
+#' #                            columnToTakeValuesFrom= 'meas',
+#' #                            rowKeyColumns= 'index')
+#'
+#'
+#' @export
 replyr_moveValuesToColumns <- function(data,
                                        columnToTakeKeysFrom,
                                        columnToTakeValuesFrom,
@@ -182,47 +181,60 @@ replyr_moveValuesToColumns <- function(data,
   if(length(intersect(c("src_mysql"),replyr_dataServiceName(data)))>0) {
     useAsChar <- FALSE
   }
-  data %>% replyr_uniqueValues(columnToTakeKeysFrom) %>%
+  data %>%
+    replyr_uniqueValues(columnToTakeKeysFrom) %>%
     replyr_copy_from(maxrow=maxcols) -> colStats
-  newCols <- colStats[[columnToTakeKeysFrom]]
+  newCols <- sort(colStats[[columnToTakeKeysFrom]])
   if(any(newCols %in% cnames)) {
     stop('replyr_moveValuesToColumns columnToTakeKeysFrom values must not include any data column names')
   }
-  mcols <- c(columnToTakeKeysFrom,columnToTakeValuesFrom)
-  copyCols <- setdiff(cnames,mcols)
-  f <- function(di) {
-    di %>% head(n=1) %>% dplyr::select(dplyr::one_of(copyCols)) -> d1
-    di %>% dplyr::select(dplyr::one_of(ucols)) -> di
-    for(ni in newCols) {
-      di %>% replyr_filter(columnToTakeKeysFrom,ni,verbose=FALSE) -> din
-      vi <- NA
-      if(replyr_nrow(din)>0) {
-        suppressWarnings({
-          # MySQL:  In .local(conn, statement, ...) : Decimal MySQL column 1 imported as numeric
-          din %>% head(n=1) %>% replyr::replyr_copy_from() %>%
-            as.data.frame() -> din1
-          vi <- din1[1,columnToTakeValuesFrom,drop=TRUE]
-        })
-      }
-      # see http://stackoverflow.com/questions/26003574/r-dplyr-mutate-use-dynamic-variable-names
-      # PostgreSQL needs to know types on character types with the lazyeval form.
-      # MySQL does not like such annotation.
-      NEWCOLNAME <- NULL  # declare not unbound
-      if(useAsChar && is.character(vi)) {
-        wrapr::let(c(NEWCOLNAME= ni),
-          d1 %>% dplyr::mutate(NEWCOLNAME = as.character(vi)) -> d1
-        )
-      } else {
-        wrapr::let(c(NEWCOLNAME= ni),
-          d1 %>% dplyr::mutate(NEWCOLNAME = vi) -> d1
-        )
-      }
-    }
-    if(eagerCompute) {
-      dplyr::compute(d1) -> d1
-    }
-    d1
+  if(length(newCols)>maxcols) {
+    stop("replyr_moveValuesToColumns too many new columns")
   }
-  gapply(data,rowKeyColumns,f,maxgroups=NULL,partitionMethod='extract')
+  let(
+    c(VCOL= columnToTakeValuesFrom),
+    (data %>%
+      dplyr::summarize(x=min(VCOL)) %>%
+      collect)$x -> minV
+  )
+  if(is.na(minV)) {
+    stop("replyr_moveValuesToColumns can't currently move NAs")
+  }
+  sentinalV <- NA
+  if(is.numeric(minV)) {
+    sentinalV <- minV - 0.1*minv - 1
+  } else if(is.character(minV)) {
+    # can fix this by padding strings on the left with a "V"
+    if(minV=='') {
+      stop("replyr_moveValuesToColumns can't currently handle blanks")
+    }
+    sentinalV <- ''
+  } else {
+    stop("replyr_moveValuesToColumns can only currently handle numeric or character data")
+  }
+  if(sentinalV>=minV) {
+    stop("replyr_moveValuesToColumns failed to pick sentinal value")
+  }
+  mcols <- c(columnToTakeKeysFrom,columnToTakeValuesFrom)
+  for(ci in newCols) {
+    wrapr::let(
+      c(NEWCOL=ci,
+        KCOL=columnToTakeKeysFrom,
+        VCOL=columnToTakeValuesFrom),
+      data %>%
+        mutate(NEWCOL = ifelse(KCOL==ci, VCOL, sentinalV)) -> data
+    )
+  }
+  copyCols <- c(setdiff(cnames,mcols),newCols)
+  data %>%
+    select(one_of(copyCols)) -> data
+  # this only works with single key column
+  wrapr::let(
+    c(KEYCOLS= rowKeyColumns),
+    data %>%
+      group_by(KEYCOLS) %>%
+      summarize_all(max) -> data
+  )
+  data
 }
 
