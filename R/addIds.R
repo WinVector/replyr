3fb8d871e09e758ee57b84ed213e88ded310bb19
John Mount <jmount@win-vector.com>
2017-05-23 08:08:09 -0700

    make addIds deterministic

--- R/addIds.R
+++ R/addIds.R
@@ -1,7 +1,6 @@
 
-#' Add unique ids to rows.
+#' Add unique ids to rows.  Note: re-arranges rows in many cases.
 #'
-#' NOT TESTED YET!
 #'
 #' @param df data.frame object to work with
 #' @param idColName name of column to add
@@ -22,13 +21,27 @@ replyr_add_ids <- function(df, idColName) {
       return(sparklyr::sdf_with_unique_id(df, id = idColName))
     }
   }
-  # SQL-style try
+  # arrange all
+  collist <- paste(colnames(df), collapse=', ')
+  colsort <- paste('dplyr::arrange(df,', collist, ')')
+  df <- eval(parse(text= colsort))
+  # dplyr style, throws if not ordered
   REPLYRIDCOLNAME <- NULL # indicate not an unbound variable
+  row_number <- function(...) { NULL } # declare not unbound function
+  # using dplyr::row_number() throws:  Error in UseMethod("escape") :
+  #   no applicable method for 'escape' applied to an object of class "function"
   wrapr::let(
     c(REPLYRIDCOLNAME= idColName),
     df %>%
-      mutate(REPLYRIDCOLNAME= 1) %>%
-      mutate(REPLYRIDCOLNAME= cumsum(REPLYRIDCOLNAME)) -> df
+      mutate(REPLYRIDCOLNAME = row_number()) -> df
   )
+  # # SQL-style try, only warns if not ordered
+  # REPLYRIDCOLNAME <- NULL # indicate not an unbound variable
+  # wrapr::let(
+  #   c(REPLYRIDCOLNAME= idColName),
+  #   df %>%
+  #     mutate(REPLYRIDCOLNAME= 1) %>%
+  #     mutate(REPLYRIDCOLNAME= cumsum(REPLYRIDCOLNAME)) -> df
+  # )
   df
 }
