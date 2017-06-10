df30b8259382910784aa9ec33e61f42ed0a368b6
John Mount <jmount@win-vector.com>
2017-06-10 14:42:29 -0700

    move checks around a bit

--- R/joinController.R
+++ R/joinController.R
@@ -47,15 +47,48 @@ tableDesription <- function(tableName,
 #' @export
 #'
 buildJoinPlan <- function(tDesc) {
+  n <- function(...) {} # declare not an unbound ref
+  count <- NULL # declare not an unbound ref
   ntab <- nrow(tDesc)
   if(length(unique(tDesc$tableName))!=ntab) {
-    stop("replyr::buildJoinPlan must have unique table names")
+    stop("replyr::buildJoinPlan must have unique table name(s)")
+  }
+  if(any(nchar(tDesc$tableName)<=0)) {
+    stop("replyr::buildJoinPlan empty table name(s)")
   }
   plans <- vector(ntab, mode='list')
   for(i in seq_len(ntab)) {
     cols <- tDesc$columns[[i]]
     keys <- tDesc$keys[[i]]
     tnam <- tDesc$tableName[[i]]
+    if(length(cols)<=0) {
+      stop(paste("replyr::buildJoinPlan table",
+                 tnam, "no columns"))
+    }
+    if(length(keys)<=0) {
+      stop(paste("replyr::buildJoinPlan table",
+                 tnam, "no keys"))
+    }
+    if(any(nchar(keys)<=0)) {
+      stop(paste("replyr::buildJoinPlan table",
+                 tnam, "empty key columns"))
+    }
+    if(length(unique(keys))!=length(keys)) {
+      stop(paste("replyr::buildJoinPlan table",
+                 tnam, "declares duplicate key columns"))
+    }
+    if(any(nchar(names(keys))<=0)) {
+      stop(paste("replyr::buildJoinPlan table",
+                 tnam, "empty key mappings"))
+    }
+    if(length(unique(names(keys)))!=length(names(keys))) {
+      stop(paste("replyr::buildJoinPlan table",
+                 tnam, "declares duplicate key mappings"))
+    }
+    if(!all(keys %in% cols)) {
+      stop(paste("replyr::buildJoinPlan table",
+                 tnam, "declares a key that is not a column"))
+    }
     abstractKeyName <- rep("", length(cols))
     keyIndexes <- match(keys, cols)
     abstractKeyName[keyIndexes] <- names(keys)
@@ -69,24 +102,89 @@ buildJoinPlan <- function(tDesc) {
   }
   plans <- dplyr::bind_rows(plans)
   # disambiguate non-key result columns
-  nonKeyIndexes <- which(nchar(plans$abstractKeyName)<=0)
-  saw <- list()
-  for(i in nonKeyIndexes) {
-    rc <- plans$resultColumn[[i]]
-    if(rc %in% names(saw)) {
-      ti <- gsub("[^a-zA-Z0-9]+", '_', plans$tableName[[i]])
-      rc <- paste(ti, rc, sep= '_')
-      plans$resultColumn[[i]] <- rc
+  dups <- plans %>%
+    dplyr::filter(nchar(plans$abstractKeyName)<=0) %>%
+    dplyr::select(resultColumn) %>%
+    dplyr::group_by(resultColumn) %>%
+    dplyr::summarize(count=n()) %>%
+    dplyr::filter(count>1)
+  if(nrow(dups)>0) {
+    for(ci in dups$resultColumn) {
+      indices <- which(plans$resultColumn==ci)
+      for(i in indices) {
+        ti <- gsub("[^a-zA-Z0-9]+", '_', plans$tableName[[i]])
+        rc <- paste(ti, ci, sep= '_')
+        plans$resultColumn[[i]] <- rc
+      }
     }
-    saw[[rc]] <- i
   }
   # catch any remaining duplication
+  nonKeyIndexes <- which(nchar(plans$abstractKeyName)<=0)
   plans$resultColumn[nonKeyIndexes] <- make.unique( plans$resultColumn[nonKeyIndexes],
                                                     sep= '_')
   plans
 }
 
-#' Execute a sequence of left joins.
+inspectJoinPlan <- function(tDesc, columnJoinPlan) {
+  resultColumn <- NULL # declare not an unbound ref
+  abstractKeyName <- NULL # declare not an unbound ref
+  tableName <- NULL # declare not an unbound ref
+  # sanity check
+  if(any(nchar(tDesc$tableName)<=0)) {
+    stop("replyr::inspectJoinPlan empty table name(s) in tDesc")
+  }
+  if(any(nchar(columnJoinPlan$tableName)<=0)) {
+    stop("replyr::inspectJoinPlan empty table name(s) in columnJoinPlan")
+  }
+  if(length(unique(tDesc$tableName)) != length(tDesc$tableName)) {
+    stop("non-unique table names in tDesc")
+  }
+  keyIdxs <- which(nchar(columnJoinPlan$abstractKeyName)>0)
+  if(!all(columnJoinPlan$resultColumn[keyIdxs]==columnJoinPlan$abstractKeyName[keyIdxs])) {
+    stop("replyr::inspectJoinPlan non-empty columnJoinPlan abstract keys must equal resultColumn")
+  }
+  # limit down to things we are using
+  columnJoinPlan <- columnJoinPlan %>%
+    dplyr::filter((nchar(resultColumn)>0) | (nchar(abstractKeyName)>0))
+  if(any(nchar(columnJoinPlan$sourceColumn)<=0)) {
+    stop("replyr::executeJoinPlan empty source column names")
+  }
+  tabsC <- unique(columnJoinPlan$tableName)
+  if(length(setdiff(tabsC, tDesc$tableName))>0) {
+    stop("replyr::executeJoinPlan tDesc does not have all the needed tables to join")
+  }
+  tDesc <- tDesc %>%
+    dplyr::filter(tableName %in% tabsC)
+  if( nrow(tDesc)<=0) {
+    stop("replyr::executeJoinPlan no tables selected")
+  }
+  tabsD <- unique(tDesc$tableName)
+  columnJoinPlan <- columnJoinPlan %>%
+    dplyr::filter(tableName %in% tabsD)
+  # check a few desired invarients of the plan
+  valCols <- columnJoinPlan$resultColumn[nchar(columnJoinPlan$abstractKeyName)<=0]
+  if(length(unique(valCols))!=length(valCols)) {
+    stop("replyr::inspectJoinPlan non-unique value columns")
+  }
+  keyCols <- unique(columnJoinPlan$abstractKeyName[nchar(columnJoinPlan$abstractKeyName)>0])
+  if(length(intersect(keyCols, valCols))>0) {
+    stop("replyr::inspectJoinPlan key columns and value columns intersect non-trivially")
+  }
+  for(i in seq_len(nrow(tDesc))) {
+    tnam <- tDesc$tableName[[i]]
+    ci <- columnJoinPlan %>%
+      dplyr::filter(tableName==tnam)
+    # don't check tDesc$keys here, as it isn't used after join plan is constructed.
+    if(!all(ci$sourceColumn %in% tDesc$columns[[i]])) {
+      stop(paste("replyr::inspectJoinPlan table",
+                 tnam, "uses a source that is not a column"))
+    }
+  }
+  list(tDesc= tDesc,
+       columnJoinPlan= columnJoinPlan)
+}
+
+#' Execute an ordered sequence of left joins.
 #'
 #' @param tDesc description of tables, from \code{\link{tableDesription}} (and likely altered by user).
 #' @param columnJoinPlan columns to join, from \code{\link{buildJoinPlan}} (and likely altered by user).
@@ -97,13 +195,25 @@ buildJoinPlan <- function(tDesc) {
 #'
 #' @examples
 #'
-#' d1 <- data.frame(id=1:3, weight= c(200, 140, 98))
-#' d2 <- data.frame(id=2:3, weight= c(130, 110))
+#'
+#' # example data
+#' d1 <- data.frame(id= 1:3,
+#'                  weight= c(200, 140, 98),
+#'                  height= c(60, 24, 12))
+#' d2 <- data.frame(pid= 2:3,
+#'                  weight= c(130, 110),
+#'                  width= 1)
+#' # get the initial description of table defs
 #' tDesc <- rbind(tableDesription('d1', d1),
 #'                tableDesription('d2', d2))
+#' # declare keys (and give them consitent names)
 #' tDesc$keys[[1]] <- list(PrimaryKey= 'id')
-#' tDesc$keys[[2]] <- list(PrimaryKey= 'id')
+#' tDesc$keys[[2]] <- list(PrimaryKey= 'pid')
+#' # build the column join plan
 #' columnJoinPlan <- buildJoinPlan(tDesc)
+#' # decide we don't want the width column
+#' columnJoinPlan$resultColumn[columnJoinPlan$resultColumn=='width'] <- ''
+#' # execute the left joins
 #' executeLeftJoinPlan(tDesc, columnJoinPlan)
 #'
 #' @export
@@ -113,16 +223,12 @@ executeLeftJoinPlan <- function(tDesc, columnJoinPlan,
                                 ...,
                                 eagerCompute= TRUE,
                                 tempNameGenerator= makeTempNameGenerator("executeLeftJoinPlan")) {
-  tabs <- unique(columnJoinPlan$tableName)
-  if(length(setdiff(tabs, tDesc$tableName))>0) {
-    stop("replyr::executeJoinPlan tDesc does not have all the needed tables to join")
-  }
-  tDesc <- tDesc %>%
-    dplyr::filter(tableName %in% tabs)
+  # sanity check
+  plans <- inspectJoinPlan(tDesc, columnJoinPlan)
+  tDesc <- plans$tDesc
+  columnJoinPlan <- plans$columnJoinPlan
+  # start joining
   ntab <- nrow(tDesc)
-  if(ntab<=0) {
-    stop("replyr::executeJoinPlan no tables selected")
-  }
   res <- NULL
   for(i in seq_len(ntab)) {
     tabnam <- tDesc$tableName[[i]]
@@ -140,6 +246,8 @@ executeLeftJoinPlan <- function(tDesc, columnJoinPlan,
     names(nmap) <- c(tableIndCol,
                      columnJoinPlan$resultColumn[keyRows],
                      columnJoinPlan$resultColumn[valRows])
+    # adding an indicator column lets us handle cases where we are taking
+    # no values.
     ti <- tDesc$handle[[i]] %>%
       addConstantColumn(tableIndCol, 1) %>%
       replyr_mapRestrictCols(nmap, restrict=TRUE)
@@ -151,7 +259,8 @@ executeLeftJoinPlan <- function(tDesc, columnJoinPlan,
       REPLYR_TABLE_PRESENT_COL <- NULL # signal not an unbound variable
       wrapr::let(
         c(REPLYR_TABLE_PRESENT_COL= tableIndCol),
-        res <- dplyr::mutate(res, REPLYR_TABLE_PRESENT_COL = ifelse(is.na(REPLYR_TABLE_PRESENT_COL), 0, 1))
+        res <- dplyr::mutate(res, REPLYR_TABLE_PRESENT_COL =
+                               ifelse(is.na(REPLYR_TABLE_PRESENT_COL), 0, 1))
       )
       if(eagerCompute) {
         res <- dplyr::compute(res, name=tempNameGenerator())
